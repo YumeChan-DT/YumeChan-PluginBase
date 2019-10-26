f72ec0419972adbb5c5feb2fe3f0c64cbdeeb58f
DJ Daemonix <dj.daemonix@nodsoft.net>
2019-10-26 20:32:23 +0200

    Implement IMessageTap Interface
    IMessageTap is to be inherited by an IPlugin Manifest, if the Plugin has features requiring the use of MessageReceived, MessageUpdated, or MessageDeleted Events.

---
+++ IMessageTap.cs
@@ -0,0 +1,13 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace Nodsoft.YumeChan.PluginBase
+{
+	public interface IMessageTap
+	{
+		Task OnMessageReceived(SocketMessage message);
+		Task OnMessageUpdated(Cacheable<IMessage, ulong> messageBeforeUpdate, SocketMessage messageAfterUpdate, ISocketMessageChannel channel);
+		Task OnMessageDeleted(Cacheable<IMessage, ulong> message, ISocketMessageChannel channel);
+	}
+}
