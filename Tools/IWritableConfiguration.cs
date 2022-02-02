fad418e476b4c445252835e37f3eadbe0095d40a
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-02-02 07:39:50 +0100

    Add IWritableConfiguration

---
+++ Tools/IWritableConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace YumeChan.PluginBase.Tools;
+
+public interface IWritableConfiguration
+{
+	public string CurrentPrefix { get; }
+
+
+	public string this[string parameter] { get; set; }
+
+	public Task LoadFromFileAsync();
+	public Task SaveToFileAsync();
+
+	public object GetValue(string parameter);
+	public T GetValue<T>(string parameter);
+
+	public void SetValue(string parameter, object value);
+	public void SetValue<T>(string parameter, T value);
+
+	public IWritableConfiguration GetSection(string sectionName);
+}
