ab7689696d8b527995fbabb101747c7e3fd4aded
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-10-07 14:47:12 +0200

    Add PluginCheck Base attributes for Slash & Context Menu commands

---
+++ YumeChan.PluginBase/Infrastructure/PluginContextCheckBaseAttribute.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace YumeChan.PluginBase.Infrastructure;
+
+/// <summary>
+/// Provides a base class for all Plugins implementing Context menu Command pre-execution checks.
+/// </summary>
+public abstract class PluginContextCheckBaseAttribute : ContextMenuCheckBaseAttribute
+{
+	/// <summary>
+	/// Error returned if this check fails.
+	/// </summary>
+	public abstract string ErrorMessage { get; protected set; }
+}
---
+++ YumeChan.PluginBase/Infrastructure/PluginSlashCheckBaseAttribute.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace YumeChan.PluginBase.Infrastructure;
+
+/// <summary>
+/// Provides a base class for all Plugins implementing Slash Command pre-execution checks.
+/// </summary>
+public abstract class PluginSlashCheckBaseAttribute : SlashCheckBaseAttribute
+{
+	/// <summary>
+	/// Error returned if this check fails.
+	/// </summary>
+	public abstract string ErrorMessage { get; protected set; }
+}
