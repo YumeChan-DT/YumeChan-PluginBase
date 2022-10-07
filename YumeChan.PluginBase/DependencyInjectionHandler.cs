a209e99583ee8c344d29c595913b0194d53cda72
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-10-07 13:22:53 +0200

    Update namespace format & Remove license headers

--- YumeChan.PluginBase/DependencyInjectionHandler.cs
+++ YumeChan.PluginBase/DependencyInjectionHandler.cs
@@ -5,26 +5,25 @@
  *	Licensed by YumeChan-DT (Nodsoft Systems) under MIT	License
  */
 
-namespace YumeChan.PluginBase
+namespace YumeChan.PluginBase;
+
+/// <summary>
+/// Startup class to register Plugin dependencies.
+/// </summary>
+public abstract class DependencyInjectionHandler
 {
 	/// <summary>
-	/// Startup class to register Plugin dependencies.
+	/// Default ctor. This should remain open and parameterless, to get instantiated by the Core.
 	/// </summary>
-	public abstract class DependencyInjectionHandler
-	{
-		/// <summary>
-		/// Default ctor. This should remain open and parameterless, to get instantiated by the Core.
-		/// </summary>
-		public DependencyInjectionHandler() { }
+	public DependencyInjectionHandler() { }
 
-		/// <summary>
-		/// Configures services for the current plugin.
-		/// </summary>
-		/// <remarks>
-		/// This method gets called before Plugin Manifest instantiation.
-		/// </remarks>
-		/// <param name="services">Service Collection to register services with.</param>
-		/// <returns>Service Collection with registered services.</returns>
-		public abstract IServiceCollection ConfigureServices(IServiceCollection services);
-	}
+	/// <summary>
+	/// Configures services for the current plugin.
+	/// </summary>
+	/// <remarks>
+	/// This method gets called before Plugin Manifest instantiation.
+	/// </remarks>
+	/// <param name="services">Service Collection to register services with.</param>
+	/// <returns>Service Collection with registered services.</returns>
+	public abstract IServiceCollection ConfigureServices(IServiceCollection services);
 }
